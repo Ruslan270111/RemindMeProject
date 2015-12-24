e14b1732f169142e0e07d0ca8d117c0ac228c845
Ruslan270111 <ruslan.baishev94@gmail.com>
2015-12-24 10:27:26 +0600

    Созданы MainActivity class и layout

---
+++ RemindMe/app/src/main/java/com/ruslan/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ruslan.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ruslan on 24.12.2015.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
