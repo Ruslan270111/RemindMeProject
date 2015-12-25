49cae748bfff0e73e1fe9fbb860aec1261af78d9
Ruslan270111 <ruslan.baishev94@gmail.com>
2015-12-25 23:15:05 +0600

    Настройка Вкладок

--- RemindMe/app/src/main/java/com/ruslan/remindme/fragment/IdeasFragment.java
+++ RemindMe/app/src/main/java/com/ruslan/remindme/fragment/IdeasFragment.java
@@ -1,23 +1,24 @@
 package com.ruslan.remindme.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ruslan.remindme.R;
 
-public class ExampleFragment extends Fragment {
+public class IdeasFragment extends AbstractTabFragment {
     private static final int LAYOUT = R.layout.fragment_example;
 
-    private View view;
-
-    public static ExampleFragment getInstance() {
+    public static IdeasFragment getInstance(Context context) {
         Bundle args = new Bundle();
-        ExampleFragment fragment = new ExampleFragment();
+        IdeasFragment fragment = new IdeasFragment();
         fragment.setArguments(args);
+        fragment.setContext(context);
+        fragment.setTitle(context.getString(R.string.tab_item_ideas));
+
 
         return fragment;
     }
@@ -27,5 +28,9 @@ public class ExampleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(LAYOUT, container, false);
         return view;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
     }
 }
