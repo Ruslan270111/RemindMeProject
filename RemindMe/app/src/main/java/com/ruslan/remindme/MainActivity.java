45d2e75eff270c1b17bab8043ca8aa30c721a681
Ruslan270111 <ruslan.baishev94@gmail.com>
2015-12-25 16:22:12 +0600

    Создание Окна Навигации

--- RemindMe/app/src/main/java/com/ruslan/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/ruslan/remindme/MainActivity.java
@@ -1,16 +1,17 @@
 package com.ruslan.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -22,7 +23,7 @@ public class MainActivity extends Activity {
         toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
-        public boolean onMenuItemClick(MenuItem menuItem) {
+            public boolean onMenuItemClick(MenuItem menuItem) {
                 return false;
             }
         });
